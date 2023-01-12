854b2337297ae20bfa58d231065392c9642f8d33
Eclipse175 <fahimkhobar2004@gmail.com>
2023-01-12 13:04:43 +0300

    adding the rest

---
+++ app.js
@@ -0,0 +1,21 @@
+const quoteDiv = document.getElementById('quoteDiv');
+const quote = document.getElementById('quote');
+const button = document.querySelector('button');
+const randomQuotes = [
+    'Work hard in silence',
+    `If you try to catch two rabbits, you'll catch none`,
+    'Pray and have Patience',
+    'If you want to try, go all the way, else dont even bother',
+    'see you <3',
+    'dummy',
+    'dummy2'
+]
+let index = 0;
+let maxLength = 7;
+button.addEventListener('click', () => {
+    quote.innerHTML = randomQuotes[index];
+    index++;
+    if (index === maxLength) {
+        location.reload();
+    }
+})
